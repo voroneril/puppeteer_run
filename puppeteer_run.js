780630b271188390e7a57938fc72fcfcfe527c15
voron <voron@voron-desktop>
2023-09-04 15:32:53 +0300

    change to ff

--- puppeteer_run.js
+++ puppeteer_run.js
@@ -14,7 +14,9 @@ module.exports = async function puppeteer_run(url) {
     if(opsys != "Windows") {
         browser = await puppeteer.launch( {
             headless: true,  //change to true in prod!
-            executablePath: '/usr/bin/google-chrome-stable'
+            //executablePath: '/usr/bin/google-chrome-stable',
+            executablePath: '/usr/bin/firefox',
+            product: 'firefox'
         }); 
     } else {
         browser = await puppeteer.launch( {
@@ -26,7 +28,9 @@ module.exports = async function puppeteer_run(url) {
     
     await page.setDefaultNavigationTimeout(0);
     
-    await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+    //await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+    
+    //await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
    
     const response = await page.goto(url);
 
